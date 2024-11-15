57a185b86d5b307c974efc884be0c2d12993b914
Troy Thomas <>
2024-11-15 02:42:19 -0600

    added recipe info

--- README.md
+++ README.md
@@ -2,5 +2,24 @@
 CLI front end for Recipe-Shop
 
 # How to run
-Run "python rs.py" in terminal from same directory as recipe-shop-cli.py file
-Must have Python installed
+Make sure you have Python 3 installed
+Make sure an instance of the Recipe-Shop server is running
+Open terminal in same folder as rs.py
+Run "pip install ascii_magic" to install ASCII Magic (used for displaying images in terminal)
+Run "python3 rs.py" to start CLI
+Use "connect [ipAddress] [port]" command to set the ip address and port number to connect to server
+Use "help" command to view all CLI commands
+
+# Info
+Recipe-Shop-CLI made by Troy Thomas
+GitHub username: TroyT21
+email thomast9@myumanitoba.ca
+
+Recipe-Shop server and web client
+https://github.com/njzfjiang/Recipe-Shop
+
+Recipe-Shop-App-Version
+https://github.com/njzfjiang/Recipe-Shop-App-Version
+
+Uses ASCII Magic to convert images to ASCII art
+https://pypi.org/project/ascii-magic/
--- rs.py
+++ rs.py
@@ -1,7 +1,8 @@
 import cmd
-import sys
+import os
 import http.client
 import json
+from ascii_magic import AsciiArt, Back
 
 #print(dir(cmd.Cmd))
 
@@ -24,39 +25,66 @@ class RecipeShopCLI(cmd.Cmd):
     
     def do_connect(self, arg):
         "Set the ip address to connect to: \"connect [ip]\"\nSet the ip address and port to connect to: \"connect [ip] [port]\""
-        connect(*parse(arg))
+        try:
+            connect(*parse(arg))
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
 
     def do_check(self,arg):
         "Check the ip address and port used for connection: \"check\""
-        checkConnect()
+        try:
+            checkConnect()
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
     
     def do_login(self, arg):
         "Login to Recipe-Shop: \"login [username] [password]\""
-        login(*parse(arg))
+        try:
+            login(*parse(arg))
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
     
     def do_logout(self, arg):
         "Logout of Recipe-Shop: \"logout\""
-        logout()
+        try:
+            logout()
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
     
     def do_user(self, arg):
         "Check if a username already exists: \"user [username]\""
-        user(arg)
+        try:
+            user(arg)
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
     
     def do_register(self, arg):
         "Register new user: \"register [username] [password] [confirmPassword]"
-        register(*parse(arg))
+        try:
+            register(*parse(arg))
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
 
     def do_search(self, arg):
-        "Search for recipes: \n\"search [keywords] [min Time] [max Time] [meal type]\"\n\"search [keywords] [min Time] [max Time]\"\n\"search [keywords] [meal type]\"\n\"search [keywords]\"\n"
-        search(*parse(arg))
+        "Search for recipes: \n\"search [keywords] [min Time] [max Time] [meal type]\"\n\"search [keywords] [min Time] [max Time]\"\n\"search [keywords] [meal type]\"\n\"search [keywords]\"\nSeparate multiple keywords with \"/\""
+        try:
+            search(*parse(arg))
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
     
     def do_recipe(self, arg):
         "View more details about a recipe: \"recipe [recipe #]\"\nMust use search command first to get recipe #"
-        recipe(arg)
+        try:
+            recipe(arg)
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
 
     def do_last(self, arg):
         "View the results from the last search command: \"last\""
-        printRecipeList()
+        try:
+            printRecipeList()
+        except:
+            print("Command failed.\nRun \"help [command]\" to check how to run it.")
 
 
 # RecipeShopCLI class
@@ -80,7 +108,7 @@ def connect(*args):
     elif(len(args) == 1):
         connect1(args[0])
     else:
-        print("Invalid number of arguments.")
+        print("Invalid number of arguments.\nRun \"help [command]\" to check how to run it.")
 
 def connect2(serverIP, serverPort):
     global ip
@@ -103,21 +131,21 @@ def checkConnect():
 def login(user, password):
     global username
     if(username == ""):
-        #print(user, password)
-        url = ip + ":" + str(port)
-        #print(url)
-        params = "/api/login?username=" + user + "&password=" + password
-        #print(params)
-        conn = http.client.HTTPConnection(url)
-        conn.request("GET", params)
-        res = conn.getresponse()
-        print(res.status, res.reason)
-        data = json.loads(res.read().decode("utf-8"))
-        if(res.status == 200):
-            print(data["message"])
-            username = user
-        else:
-            print(data["error"])
+        try:
+            url = ip + ":" + str(port)
+            params = "/api/login?username=" + user + "&password=" + password
+            conn = http.client.HTTPConnection(url)
+            conn.request("GET", params)
+            res = conn.getresponse()
+            print(res.status, res.reason)
+            data = json.loads(res.read().decode("utf-8"))
+            if(res.status == 200):
+                print(data["message"])
+                username = user
+            else:
+                print(data["error"])
+        except:
+            print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
     else:
         print("Already logged in as", username)
 
@@ -130,46 +158,52 @@ def logout():
         username = ""
 
 def user(user):
-    url = ip + ":" + str(port)
-    params = "/api/user-exist?username=" + user
-    conn = http.client.HTTPConnection(url)
-    conn.request("GET", params)
-    res = conn.getresponse()
-    print(res.status, res.reason)
-    data = json.loads(res.read().decode("utf-8"))
-    if(res.status == 200):
-        if(data["exists"]):
-            print("Username", user, "already in use")
+    try:
+        url = ip + ":" + str(port)
+        params = "/api/user-exist?username=" + user
+        conn = http.client.HTTPConnection(url)
+        conn.request("GET", params)
+        res = conn.getresponse()
+        print(res.status, res.reason)
+        data = json.loads(res.read().decode("utf-8"))
+        if(res.status == 200):
+            if(data["exists"]):
+                print("Username", user, "already in use")
+            else:
+                print("Username", user, "is available")
         else:
-            print("Username", user, "is available")
-    else:
-        print(data["error"])
+            print(data["error"])
+    except:
+        print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
 
 def register(user, password, confirm):
     global username
     if(username == ""):
-        url = ip + ":" + str(port)
-        params = "/api/register"
-        body = json.dumps({
-            "username": user,
-            "password": password,
-            "confirmPassword": confirm
-        })
-        print(body)
-        headers = {
-            "Content-Type": "application/json",
-            "Content-Length": len(body)
-        }
-        print(headers)
-        conn = http.client.HTTPConnection(url)
-        conn.request("POST", params, body, headers)
-        res = conn.getresponse()
-        print(res.status, res.reason)
-        data = json.loads(res.read().decode("utf-8"))
-        if(res.status == 201):
-            print(data["message"])
-        else:
-            print(data["error"])
+        try:
+            url = ip + ":" + str(port)
+            params = "/api/register"
+            body = json.dumps({
+                "username": user,
+                "password": password,
+                "confirmPassword": confirm
+            })
+            #print(body)
+            headers = {
+                "Content-Type": "application/json",
+                "Content-Length": len(body)
+            }
+            #print(headers)
+            conn = http.client.HTTPConnection(url)
+            conn.request("POST", params, body, headers)
+            res = conn.getresponse()
+            print(res.status, res.reason)
+            data = json.loads(res.read().decode("utf-8"))
+            if(res.status == 201):
+                print(data["message"])
+            else:
+                print(data["error"])
+        except:
+            print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
     else:
         print("Already logged in as", username)
         print("Please logout before registering a new user.")
@@ -184,24 +218,27 @@ def search(*args):
     elif(len(args) == 1):
         search1(args[0])
     else:
-        print("Invalid number of arguments.")
+        print("Invalid number of arguments.\nRun \"help [command]\" to check how to run it.")
 
 def search4(keywords, minTime, maxTime, mealType):
     if(keywords != ""):
         if(minTime < maxTime):
-            url = ip + ":" + str(port)
-            params = "/api/recipe/search?keyword=" + keywords + "&mealType=" + mealType + "&time=" + minTime + "-" + maxTime
-            print(params)
-            conn = http.client.HTTPConnection(url)
-            conn.request("GET", params)
-            res = conn.getresponse()
-            print(res.status, res.reason)
-            data = res.read()
-            if(res.status == 200):
-                #print(data[:250], "...")
-                getRecipeList(json.loads(data))
-            else:
-                print(data)
+            try:
+                url = ip + ":" + str(port)
+                params = "/api/recipe/search?keyword=" + str(keywords).replace("/","+") + "&mealType=" + mealType + "&time=" + minTime + "-" + maxTime
+                #print(params)
+                conn = http.client.HTTPConnection(url)
+                conn.request("GET", params)
+                res = conn.getresponse()
+                print(res.status, res.reason)
+                data = res.read()
+                if(res.status == 200):
+                    #print(data[:250], "...")
+                    getRecipeList(json.loads(data))
+                else:
+                    print(data)
+            except:
+                print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
         else:
             search2(keywords, mealType)
     else:
@@ -210,9 +247,33 @@ def search4(keywords, minTime, maxTime, mealType):
 def search3(keywords, minTime, maxTime):
     if(keywords != ""):
         if(minTime < maxTime):
+            try:
+                url = ip + ":" + str(port)
+                params = "/api/recipe/search?keyword=" + keywords + "&time=" + minTime + "-" + maxTime
+                #print(params)
+                conn = http.client.HTTPConnection(url)
+                conn.request("GET", params)
+                res = conn.getresponse()
+                print(res.status, res.reason)
+                data = res.read()
+                if(res.status == 200):
+                    #print(data[:250], "...")
+                    getRecipeList(json.loads(data))
+                else:
+                    print(data)
+            except:
+                print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
+        else:
+            search1(keywords)
+    else:
+        print("Please enter keywords and try again.")
+
+def search2(keywords, mealType):
+    if(keywords != ""):
+        try:
             url = ip + ":" + str(port)
-            params = "/api/recipe/search?keyword=" + keywords + "&time=" + minTime + "-" + maxTime
-            print(params)
+            params = "/api/recipe/search?keyword=" + keywords + "&mealType=" + mealType
+            #print(params)
             conn = http.client.HTTPConnection(url)
             conn.request("GET", params)
             res = conn.getresponse()
@@ -223,44 +284,29 @@ def search3(keywords, minTime, maxTime):
                 getRecipeList(json.loads(data))
             else:
                 print(data)
-        else:
-            search1(keywords)
-    else:
-        print("Please enter keywords and try again.")
-
-def search2(keywords, mealType):
-    if(keywords != ""):
-        url = ip + ":" + str(port)
-        params = "/api/recipe/search?keyword=" + keywords + "&mealType=" + mealType
-        print(params)
-        conn = http.client.HTTPConnection(url)
-        conn.request("GET", params)
-        res = conn.getresponse()
-        print(res.status, res.reason)
-        data = res.read()
-        if(res.status == 200):
-            #print(data[:250], "...")
-            getRecipeList(json.loads(data))
-        else:
-            print(data)
+        except:
+            print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
     else:
         print("Please enter keywords and try again.")
 
 def search1(keywords):
     if(keywords != ""):
-        url = ip + ":" + str(port)
-        params = "/api/recipe/search?keyword=" + keywords
-        print(params)
-        conn = http.client.HTTPConnection(url)
-        conn.request("GET", params)
-        res = conn.getresponse()
-        print(res.status, res.reason)
-        data = res.read()
-        if(res.status == 200):
-            #print(data[:250], "...")
-            getRecipeList(json.loads(data))
-        else:
-            print(data)
+        try:
+            url = ip + ":" + str(port)
+            params = "/api/recipe/search?keyword=" + keywords
+            #print(params)
+            conn = http.client.HTTPConnection(url)
+            conn.request("GET", params)
+            res = conn.getresponse()
+            print(res.status, res.reason)
+            data = res.read()
+            if(res.status == 200):
+                #print(data[:250], "...")
+                getRecipeList(json.loads(data))
+            else:
+                print(data)
+        except:
+            print("Failed to connect.\nRun \"check\" command to see ip\nRun \"connect\" command to change ip")
     else:
         print("Please enter keywords and try again.")
 
@@ -297,6 +343,24 @@ def recipe(arg):
     index = int(arg)
     if(index >= 0 and index < len(recipes)):
         print("\n", recipes[index]["recipe"]["label"], sep="")
+        print("Recipe from", recipes[index]["recipe"]["source"], "\n")
+        try:
+            print("Image:")
+            image = AsciiArt.from_url(recipes[index]["recipe"]["image"])
+            image.to_terminal(min(os.get_terminal_size().columns,os.get_terminal_size().lines*2))
+
+            #image.to_html_file('image.html', columns=200)
+            #filePath = "file:///Recipe-Shop-CLI/image.html"
+            #print("\n", "Better Image (opens in browser):\n", filePath, sep="")
+        except OSError as e:
+            print("\nFailed to load image.")
+        print("\nIngredients:")
+        for i in range(len(recipes[index]["recipe"]["ingredientLines"])):
+            print("  \u2022", recipes[index]["recipe"]["ingredientLines"][i])
+        print("\nInstructions:")
+        print(recipes[index]["recipe"]["url"])
+    else:
+        print("Invalid recipe #.\nCheck last search with \"last\" command, or make a new search with \"search\" command.")
 
 
     
